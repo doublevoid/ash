f939c548fd1b8595ba82d2e60a805d297c038268
doublevoid <gabrielgarocs@gmail.com>
2023-12-20 13:26:16 -0300

    add unique constraint to changeset

--- lib/ash/votes/post_vote.ex
+++ lib/ash/votes/post_vote.ex
@@ -17,6 +17,7 @@ defmodule Ash.Votes.PostVote do
     |> cast(attrs, [:value, :post_id, :user_id])
     |> validate_required([:value, :post_id, :user_id])
     |> validate_number(:value, greater_than: -2, less_than: 2)
+    |> unique_constraint(:post_id, name: :post_votes_post_id_user_id_index)
     |> assoc_constraint(:user)
     |> assoc_constraint(:post)
   end
