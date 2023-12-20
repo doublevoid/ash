3dac99fcac630d955615875526b9e25c0e5613bc
doublevoid <gabrielgarocs@gmail.com>
2023-12-20 13:02:40 -0300

    associations for post vote

--- lib/ash/votes/post_vote.ex
+++ lib/ash/votes/post_vote.ex
@@ -5,13 +5,19 @@ defmodule Ash.Votes.PostVote do
   schema "post_votes" do
     field :value, :integer
 
+    belongs_to :post, Ash.Discussions.Post
+    belongs_to :user, Ash.Discussions.User
+
     timestamps(type: :utc_datetime)
   end
 
   @doc false
   def changeset(post_vote, attrs) do
     post_vote
-    |> cast(attrs, [:value])
-    |> validate_required([:value])
+    |> cast(attrs, [:value, :post_id, :user_id])
+    |> validate_required([:value, :post_id, :user_id])
+    |> validate_number(:value, greater_than: -2, less_than: 2)
+    |> assoc_constraint(:user)
+    |> assoc_constraint(:post)
   end
 end
