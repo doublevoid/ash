aa8b8d4947600f2bdcaa894c59d730a64c580aef
doublevoid <gabrielgarocs@gmail.com>
2024-01-17 18:12:40 -0300

    improvements to timeline comment component

--- lib/ash_web/components/user_timeline_comment_component.ex
+++ lib/ash_web/components/user_timeline_comment_component.ex
@@ -3,12 +3,19 @@ defmodule AshWeb.Components.UserTimelineCommentComponent do
 
   def comment(assigns) do
     ~H"""
-    <div class="flex flex-col">
-      <p>
-        <%= @comment.title || @comment.post.title %> in <%= if @comment.community,
-          do: @comment.community.name,
-          else: @comment.post.community.name %>
-      </p>
+    <div class="flex flex-col grow">
+      <div class="flex flex-row">
+        <.link patch={~p"/c/#{@comment.community.name}/comments/#{@comment.post_id}"}>
+          <%= @comment.title %>
+        </.link>
+        <p class="text-sm">
+          in
+          <.link patch={~p"/c/#{@comment.community.name}"}>
+            <%= @comment.community.name %>
+          </.link>
+          at <%= @comment.inserted_at %>
+        </p>
+      </div>
       <p><%= @comment.body %></p>
       <p>/u/<%= @comment.user.username %></p>
     </div>
