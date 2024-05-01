d94fa464934e86e75ba297a2c44a2e213d0446bd
doublevoid <gabrielgarocs@gmail.com>
2024-05-01 20:02:51 -0300

    fix hr

--- lib/ash_web/live/components/timeline_component.ex
+++ lib/ash_web/live/components/timeline_component.ex
@@ -12,18 +12,21 @@ defmodule AshWeb.Components.TimelineComponent do
         class="flex flex-col"
         phx-viewport-bottom="load-more"
       >
-        <div :for={{_key, post} <- @posts} class="flex flex-row">
-          <div class="basis-12">
-            <.live_component
-              module={VoteComponent}
-              id={"vote-post-#{post.id}"}
-              current_user={@current_user}
-              voteable={post}
-            />
-          </div>
-          <div>
-            <PostComponent.post post={post} />
-         </div>
+        <div :for={{_key, post} <- @posts} class="border-y-1">
+          <div class="flex flex-row">
+            <div class="basis-12">
+              <.live_component
+                module={VoteComponent}
+                id={"vote-post-#{post.id}"}
+                current_user={@current_user}
+                voteable={post}
+              />
+            </div>
+            <div>
+              <PostComponent.post post={post} />
+            </div>
+           </div>
+          <hr />
         </div>
       </div>
     </div>
